30bb56a1cc2bcaa9deb4f5f75e26a1683bf6d3c7
Grawor <41929192+Grawor@users.noreply.github.com>
2020-04-11 10:36:15 +0900

    プロジェクト ファイルを追加します。

---
+++ PcCameraApp/MainForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PcCameraApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(291, 32);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(552, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FPS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1500, 800);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1576, 936);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "PCカメラ映像取得ソフト";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PcCameraApp/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;             // AForge.NETライブラリから読込
+using AForge.Video.DirectShow;  // AForge.NETライブラリから読込 
+
+using OpenCvSharp;              // OpenCVSharp
+
+
+namespace PcCameraApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // フィールド
+        public bool DeviceExist = false;                // デバイス有無
+        public FilterInfoCollection videoDevices;       // カメラデバイスの一覧
+        public VideoCaptureDevice videoSource = null;   // カメラデバイスから取得した映像
+
+        // Loadイベント（Formの立ち上げ時に実行）
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.getCameraInfo();
+        }
+
+        // カメラ情報の取得
+        public void getCameraInfo()
+        {
+            try
+            {
+                // 端末で認識しているカメラデバイスの一覧を取得
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                comboBox1.Items.Clear();
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    // カメラデバイスの一覧をコンボボックスに追加
+                    comboBox1.Items.Add(device.Name);
+                    comboBox1.SelectedIndex = 0;
+                    DeviceExist = true;
+                }
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                comboBox1.Items.Add("Deviceが存在していません。");
+            }
+        }
+
+        // 開始or停止ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("ボタンクリック");
+
+            if (button1.Text == "開始")
+            {
+
+                if (DeviceExist)
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+                    videoSource.NewFrame += new NewFrameEventHandler(videoRendering);
+                    this.CloseVideoSource();
+
+                    videoSource.Start();
+
+                    button1.Text = "停止";
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    label1.Text = "デバイスが存在していません。";
+                }
+            }
+            else
+            {
+                if (videoSource.IsRunning)
+                {
+                    timer1.Enabled = false;
+                    this.CloseVideoSource();
+                    label1.Text = "停止中";
+                    button1.Text = "開始";
+
+                }
+            }
+        }
+        // 描画処理
+        private void videoRendering(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = img;
+
+        }
+        // 停止の初期化
+        private void CloseVideoSource()
+        {
+            if (!(videoSource == null))
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource = null;
+                }
+        }
+        // フレームレートの取得
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = videoSource.FramesReceived.ToString() + "FPS";
+        }
+    }
+}
